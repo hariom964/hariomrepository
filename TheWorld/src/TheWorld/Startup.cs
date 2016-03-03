57214fd846ccdc70a51e48741f3b2cf68137d1c5
hariom3pillar <hariom.sharma@3pillarglobal.com>
2016-03-03 18:17:13 +0530

    no message

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -24,7 +24,7 @@ namespace TheWorld
             Configuration = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
-       // // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
